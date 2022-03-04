29cac9eca3f853d54bb7ea920ed63e4b49583ed4
Charles Woods <charles.woods48@yahoo.com>
2022-03-04 16:21:35 -0600

    reviews table on database

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .db import db
 from .user import User
 from .developer import Developer
+from .review import Review
---
+++ app/models/review.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from .db import db
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text, nullable=False)
+    userId = db.Column(db.Integer,  db.ForeignKey('users.id'), nullable=False)
+    developerId = db.Column(db.Integer, db.ForeignKey('developers.id'), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    
+    

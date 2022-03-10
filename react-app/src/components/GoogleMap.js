50ce2238c88d1489985c8a9bfea3995ee6aa8528
jacob <jacoberichardson7@gmail.com>
2022-03-10 11:35:57 -0700

    git

--- react-app/src/components/GoogleMap.js
+++ react-app/src/components/GoogleMap.js
@@ -4,33 +4,37 @@ import GoogleMapReact from 'google-map-react';
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class SimpleMap extends Component {
-    static defaultProps = {
-        center: {
-            lat: 59.95,
-            lng: 30.33
-        },
-        zoom: 11
-    };
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
 
-    render() {
-        return (
-            // Important! Always set the container height explicitly
-            <div style={{ height: '100vh', width: '100%' }}>
-                <GoogleMapReact
-                    // change key to your API key
-                    bootstrapURLKeys={{ key: 'AIzaSyBiQq5Z8o8i2sbzKNIitbGVQ27bWWuw23I' }}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
-                >
-                    <AnyReactComponent
-                        lat={59.955413}
-                        lng={30.337844}
-                        text="My Marker"
-                    />
-                </GoogleMapReact>
-            </div>
-        );
-    }
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <>
+        <div>{this.props.city}</div>
+        <div>{this.props.state}</div>
+        <div style={{ height: '100vh', width: '100%' }}>
+          <GoogleMapReact
+            // change key to your API key
+            bootstrapURLKeys={{ key: 'AIzaSyBiQq5Z8o8i2sbzKNIitbGVQ27bWWuw23I' }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            <AnyReactComponent
+              lat={59.955413}
+              lng={30.337844}
+              text="My Marker"
+            />
+          </GoogleMapReact>
+        </div>
+      </>
+    );
+  }
 }
 
 export default SimpleMap;

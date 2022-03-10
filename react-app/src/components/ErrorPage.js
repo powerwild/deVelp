bae9f550936b7eb6f8125100898f2477af69033a
jacob <jacoberichardson7@gmail.com>
2022-03-09 17:48:11 -0700

    404 page

--- react-app/src/components/ErrorPage.js
+++ react-app/src/components/ErrorPage.js
@@ -1,1 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+function ErrorPage() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true)
+  }, [])
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <div className="error-video">
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+    </div>
+  )
+}
+
+export default ErrorPage;

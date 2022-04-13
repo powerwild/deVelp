05612ba30659fbb3db75070908dceeb5d5cc4912
Charles Woods <charles.woods48@yahoo.com>
2022-04-13 11:57:16 -0500

    updates

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux'
 import { signUp } from '../../store/session';
 

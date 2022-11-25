8b2d67738664d56da57411d4b0e0678b483876f9
Casey Spears <92146309+powerwild@users.noreply.github.com>
2022-11-25 14:25:42 -0600

    Update b7fb186ae4fe_.py

--- migrations/versions/b7fb186ae4fe_.py
+++ migrations/versions/b7fb186ae4fe_.py
@@ -64,7 +64,11 @@ def upgrade():
     sa.PrimaryKeyConstraint('id')
     )
     if environment == "production":
-        op.execute(f"ALTER TABLE <table_name> SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE skills SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE developers SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE devskills SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE reviews SET SCHEMA {SCHEMA};")
 
 
 
